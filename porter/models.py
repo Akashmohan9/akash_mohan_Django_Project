f2dee989ce5d6709dfb289e0308cfd4950316b8b
Akashmohan9 <akashmohanaachi@gmail.com>
2025-05-01 08:27:50 +0530

    01stMay'25Sync

---
+++ porter/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+#django:-
+#D-b:- sub-module
+#models:- main modules to use
+
+# Create your models here.
+
+class Product(models.Model):
+    product_id =models.AutoField
---
+++ porter/views.py
@@ -0,0 +1,39 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# Types of views in python Django:- Function-Based & class-based view
+
+#Function Based:- A python function that takes a web request and returns a web response.
+#class based:- They provide an object-oriented way of organising your view code.
+
+# Function Based view:
+def index(request):
+    return render(request,'index.html')
+# return HttpResponse('Hello, Mate! This is Index Page')
+
+#class based view:
+from django.views import View
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse('This is About Page.')
+    
+def skills(request):
+    return render(request,'skills.html')
+    # return HttpResponse('This is About My Skills!')   
+
+def certifications(request):
+    return render(request,'certifications.html')
+    # return HttpResponse('This is About My Certifications!')
+
+def qualifications(request):
+    return render(request,'qualifications.html')
+    # return HttpResponse('This is About My Qualifications!')
+
+def projects(request):
+    return render(request,'projects.html')
+    # return HttpResponse('This is About My Projects ')
